bea423fd644b4908c9886930c84104dbe9ab2372
HMM Thasneem <137602501+thasneem36@users.noreply.github.com>
2025-04-07 23:40:21 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and 
+          Edit <code>src/App.jsx</code> and Thasneem
         </p>
       </div>
       <p className="read-the-docs">
