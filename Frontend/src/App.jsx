ceffb513bb473f31a567b75402c0169bbe43a6b3
HMM Thasneem <137602501+thasneem36@users.noreply.github.com>
2025-04-09 00:08:09 +0530

    update nav bar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,10 @@ import Attendance from './pages/Admin/Manage/Attendance'
 import TimeOff from './pages/Admin/Manage/TimeOff'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Profile from './pages/Settings/Profile'
+import LeaveSettings from './pages/Settings/LeaveSettings'
+import Calendar from './pages/Settings/Calendar'
+import InOutTime from './pages/Settings/InOutTime'
 
 
 function App() {
@@ -23,6 +27,12 @@ function App() {
           <Route path='/attendance' element={<Attendance />} />
           <Route path='/timeOff' element={<TimeOff />} />
           
+          {/* settings route */}
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/leaveSettings' element={<LeaveSettings />} />
+          <Route path='/calendar' element={<Calendar />} />
+          <Route path='/inOutTime' element={<InOutTime />} />
+
         </Routes>
 
       </BrowserRouter>
